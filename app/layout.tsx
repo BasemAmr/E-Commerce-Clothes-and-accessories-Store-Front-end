c8f60980b8929e1732de1fa71725fd410a2a5417
BasemAmr <1.million.arab.coders@gmail.com>
2024-12-10 02:24:34 +0200

    Initial commit from Create Next App

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Urbanist } from 'next/font/google';
+import "./globals.css";
+import Footer from "../components/Footer";
+const font = Urbanist({
+  subsets: ["latin"],
+})
+// ecommerce shop
+export const metadata: Metadata = {
+  title: "Ecommerce Shop",
+  description: "An ecommerce shop built with Next.js and Stripe",
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${ font.className }`}
+      >
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
