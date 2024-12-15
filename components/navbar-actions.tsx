df47e4e02157da18f20064867c4aee686c616e50
BasemAmr <1.million.arab.coders@gmail.com>
2024-12-15 20:41:15 +0200

    git isn't only a cli !

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -10,7 +10,7 @@ import { useRouter } from "next/navigation";
 const NavbarActions = () => {
     const [isMounted, setIsMounted] = useState(false);
 
-
+// 
     const router = useRouter();
 
     const cart = useCart();
