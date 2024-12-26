ce10b14a7a8adb96ce207ee08c161c87a0381e07
BasemAmr <1.million.arab.coders@gmail.com>
2024-12-26 22:44:41 +0200

    Reduce revalidation time for API fetches from 10 to 3 seconds across multiple actions

--- actions/get-billboards.tsx
+++ actions/get-billboards.tsx
@@ -3,7 +3,7 @@ import { Billboard } from "@/types/types";
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/billboards`;
 
 const getBillboard = async (id:string): Promise<Billboard> => {
-    const res = await fetch(`${URL}/${id}`, { cache: 'force-cache', next: { revalidate: 10 } });
+    const res = await fetch(`${URL}/${id}`, { cache: 'force-cache', next: { revalidate: 3 } });
 
     return res.json();
 }
--- actions/get-categories.tsx
+++ actions/get-categories.tsx
@@ -3,7 +3,7 @@ import { Category } from "@/types/types";
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
 
 const getCategories = async (): Promise<Category[]> => {
-    const res = await fetch(URL, { cache: 'force-cache', next: { revalidate: 10 } });
+    const res = await fetch(URL, { cache: 'force-cache', next: { revalidate: 3 } });
 
     return res.json();
 }
